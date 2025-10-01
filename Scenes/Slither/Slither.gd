8c54d0ce5869a75a37273a3d940fec3c3d20a9dd
Kagula19 <baciu.gabriel09102006@gmail.com>
2025-10-02 01:58:44 +0200

    NewStateMachineSlither

--- Scenes/Slither/Slither.gd
+++ Scenes/Slither/Slither.gd
@@ -2,17 +2,25 @@ extends CharacterBody2D
 var speed : int = 150
 var dir = 1
 var gravity = ProjectSettings.get_setting('physics/2d/default_gravity')
-@onready var ray_cast_2d_right: RayCast2D = $RayCast2DRight
-@onready var ray_cast_2d_left: RayCast2D = $RayCast2DLeft
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 	velocity.y = gravity
 	velocity.x = speed * dir
-	if ray_cast_2d_left.is_colliding():
-		dir = dir * -1
-		animated_sprite_2d.scale.x = -1 
-	if ray_cast_2d_right.is_colliding():
-		dir = dir * -1
-		animated_sprite_2d.scale.x = -1 
+
+
+
+func _on_front_trigger_body_entered(body: Node2D) -> void:
+	if body.is_in_group('Walls'):
+		dir *= -1
+		animated_sprite_2d.flip_h = not animated_sprite_2d.flip_h
+
+
+
+
+func _on_ledge_trigger_body_exited(body: Node2D) -> void:
+	if body.is_in_group('Walls'):
+		dir *= -1
+		animated_sprite_2d.flip_h = not animated_sprite_2d.flip_h
