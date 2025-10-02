f63bcd0d00a5fb6521120a0b4bea3e750189e3c6
Kagula19 <baciu.gabriel09102006@gmail.com>
2025-10-02 13:45:56 +0200

    SlitherTriggerFix

--- Scenes/Slither/Slither.gd
+++ Scenes/Slither/Slither.gd
@@ -2,10 +2,8 @@ extends CharacterBody2D
 var speed : int = 150
 var dir = 1
 var gravity = ProjectSettings.get_setting('physics/2d/default_gravity')
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity.y = gravity
 	velocity.x = speed * dir
 	move_and_slide()
@@ -15,7 +13,9 @@ func _physics_process(delta: float) -> void:
 func _on_front_trigger_body_entered(body: Node2D) -> void:
 	if body.is_in_group('Map'):
 		dir *= -1
-		animated_sprite_2d.flip_h = not animated_sprite_2d.flip_h
+		scale.x *= -1
+
+
 
 
 
@@ -25,5 +25,4 @@ func _on_ledge_trigger_body_exited(body: Node2D) -> void:
 		return
 	if body.is_in_group('Map'):
 		dir *= -1
-		animated_sprite_2d.flip_h = not animated_sprite_2d.flip_h
-		
+		scale.x *= -1
