4adeb5ae1848babc1b3a0a295d769a4f23590e45
ToXer04 <simonefarina2004@gmail.com>
2025-10-02 12:55:51 +0200

    Player Jump States + TileSet Collisions Fix

---
+++ Scenes/MC/State_MC_JumpStart.gd
@@ -0,0 +1,19 @@
+extends StateMachineState
+
+@onready var sprite: AnimatedSprite2D = %AnimatedSprite2D
+@onready var state_machine: StateMachine = %StateMachine
+
+# Called when the state machine enters this state.
+func _enter_state() -> void:
+	sprite.play("JumpStart")
+
+
+# Called every frame when this state is active.
+func _process(delta: float) -> void:
+	if not sprite.is_playing(): # animazione finita
+		state_machine.set_current_state($"../JumpRise")
+
+
+# Called when the state machine exits this state.
+func _exit_state() -> void:
+	pass
