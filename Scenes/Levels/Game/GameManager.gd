4751bbd67f86331e99e6561ecdf04ad99f4c1e7d
ToXer04 <simonefarina2004@gmail.com>
2025-10-14 23:12:26 +0200

    Build v43

--- Scenes/Levels/Game/GameManager.gd
+++ Scenes/Levels/Game/GameManager.gd
@@ -10,23 +10,25 @@ func _ready():
 		print("Sono Host!")
 		spawn_players()  # solo l'host fa spawn!
 
-func spawn_players():
-	for peer_id in multiplayer.get_peers():
-		_spawn_player.rpc(peer_id) # tutti i client
-	_spawn_player(multiplayer.get_unique_id()) # spawn locale host
+# GameManager.gd
 
-
-func spawn_players_steam():
-	for steam_id in SteamLobbyManager.get_lobby_members():
-		_spawn_player(steam_id)
-
-@rpc("authority") 
-func _spawn_player(peer_id): 
-	print("Spawno per: ", peer_id) 
-	var player = preload("res://Scenes/MC/Player.tscn").instantiate() 
-	player.name = "Player_%s" % peer_id 
-	player.set_multiplayer_authority(peer_id) 
+@rpc("authority", "call_local")
+func _spawn_player(peer_id):
+	var player = preload("res://Scenes/MC/Player.tscn").instantiate()
+	player.name = "Player_%s" % peer_id
+	player.set_multiplayer_authority(peer_id)
 	players.add_child(player)
+
+func spawn_players():
+	# spawn host
+	_spawn_player(multiplayer.get_unique_id())
+
+	# spawn tutti i client sul host
+	for peer_id in multiplayer.get_peers():
+		rpc_id(peer_id, "_spawn_player", peer_id) # manda RPC al client
+		# contemporaneamente spawn anche sul host
+		_spawn_player(peer_id)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
