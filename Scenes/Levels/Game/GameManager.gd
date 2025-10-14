db2fb5819a0d1ef5edeb40a8e47113df607850cc
ToXer04 <simonefarina2004@gmail.com>
2025-10-14 21:51:58 +0200

    Build v37

--- Scenes/Levels/Game/GameManager.gd
+++ Scenes/Levels/Game/GameManager.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var players: Node2D = $Players
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,17 +19,14 @@ func spawn_players_steam():
 	for steam_id in SteamLobbyManager.get_lobby_members():
 		_spawn_player(steam_id)
 
-@rpc("authority", "call_local")
-func _spawn_player(peer_id):
-	print("Spawno per: ", peer_id)
-
-	# Usa MultiplayerSpawner per creare il player
-	var spawner = $MultiplayerSpawner  # Assicurati che esista nella scena
-	var player = spawner.spawn("player")  # "player" è lo Spawn ID
-
-	player.name = "Player_%s" % peer_id
-	player.set_multiplayer_authority(peer_id)
-	player.global_position = Vector2(7000.0, 2800.0)
+@rpc("authority", "call_local") 
+func _spawn_player(id): 
+	print("Spawno per: ", id) 
+	var player = preload("res://Scenes/MC/Player.tscn").instantiate() 
+	player.name = "Player_%s" % id 
+	player.set_multiplayer_authority(id) 
+	player.global_position = Vector2(7000.0, 2800.0) 
+	players.add_child(player)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
