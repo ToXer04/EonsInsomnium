5705c118c9b7a54d5989532295704bbee8339c61
ToXer04 <simonefarina2004@gmail.com>
2025-11-11 18:31:19 +0100

    Change MC v21 (Success)

--- Scenes/Levels/Game/GameManager.gd
+++ Scenes/Levels/Game/GameManager.gd
@@ -5,33 +5,20 @@ extends Node2D
 # Questo è il nodo Spawner che hai già nella scena
 @onready var spawner: MultiplayerSpawner = %MultiplayerSpawner
 
+func _ready() -> void:
+	for i in 100:
+		await get_tree().process_frame
+	if multiplayer.is_server():
+		print("Sono Host")
+		spawnPlayer(1, Singleton.selectedChar)
+	else:
+		print("Sono Client")
+		rpc_id(1, "spawnPlayer", multiplayer.get_unique_id(), Singleton.selectedChar)
 
-func _ready():
-	print("Level Ready!")
-	if not multiplayer.is_server():
-		return
-
-	multiplayer.peer_connected.connect(add_player)
-	multiplayer.peer_disconnected.connect(del_player)
-
-	for id in multiplayer.get_peers():
-		add_player(id)
-	add_player(1)
-
-func add_player(id: int):
-	print("Add player: " + str(id))
-	var character = load("res://Scenes/MC/Eon/Eon.tscn").instantiate()
-	character.name = str(id)
-	character.set_multiplayer_authority(id)
-	players.add_child(character)
-
-func del_player(id: int):
-	if not players.has_node(str(id)):
-		return
-	players.get_node(str(id)).queue_free()
-
-func _exit_tree():
-	if not multiplayer.is_server():
-		return
-	multiplayer.peer_connected.disconnect(add_player)
-	multiplayer.peer_disconnected.disconnect(del_player)
+@rpc("any_peer", "call_remote")
+func spawnPlayer(id: int, character: String):
+	print("Spawn per " +  str(id))
+	var path : String = "res://Scenes/MC/%s/%s.tscn" % [character, character]
+	var player = load(path).instantiate()
+	player.name = character + str(id)
+	players.add_child(player)
